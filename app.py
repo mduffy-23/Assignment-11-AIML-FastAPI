491b70696bbae3ea9ad598c191dec9790dd978d3
Matthew Duffy <mduffy23@hotmail.com>
2025-11-15 14:23:39 -0500

    Add Files From Assignment

---
+++ app.py
@@ -0,0 +1,51 @@
+# Packages
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from pydantic import BaseModel
+import xgboost as xgb
+import numpy as np
+
+class SalesLagFeatures(BaseModel):
+    Sales_M_1: float
+    Sales_M_2: float
+    Sales_M_3: float
+    Sales_M_4: float
+    Sales_M_5: float
+    Sales_M_6: float
+
+model = xgb.Booster()
+model.load_model("model/model.json")
+
+feature_names = [
+    "Sales_M_1",
+    "Sales_M_2",
+    "Sales_M_3",
+    "Sales_M_4",
+    "Sales_M_5",
+    "Sales_M_6"
+]
+
+# FastAPI
+## Create FastAPI instance
+app = FastAPI()
+
+## Serve static files
+app.mount("/static", StaticFiles(directory="frontend", html=True), name="static")
+
+
+## Define prediction endpoint
+@app.post("/predict")
+def predict(data: SalesLagFeatures):
+    input_data = np.array([
+        data.Sales_M_1,
+        data.Sales_M_2,
+        data.Sales_M_3,
+        data.Sales_M_4,
+        data.Sales_M_5,
+        data.Sales_M_6
+    ]).reshape(1, -1)
+
+    dmatrix = xgb.DMatrix(input_data, feature_names=feature_names)
+    prediction = float(model.predict(dmatrix)[0])
+
+    return {"prediction": round(prediction, 2)}
